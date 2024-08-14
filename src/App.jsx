9987843aa9f61ede1587c275d1079fd161757591
qnchoyy <qni191488@abv.bg>
2024-08-14 19:29:02 +0300

    implement Register

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </>
   );
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,32 +1,35 @@
 import { useState } from "react";
 import { auth } from "../../config/firebase";
-import { signInWithEmailAndPassword } from "firebase/auth";
-
-import "./Login.css";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { Link, useNavigate } from "react-router-dom";
 
-export default function Login() {
+export default function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
   const navigate = useNavigate();
 
-  const handleLogin = async () => {
-    if (!email || !password) {
-      alert("Please enter both email and password.");
+  const handleRegister = async () => {
+    if (!email || !password || !rePassword) {
+      alert("All fields are required.");
+      return;
+    }
+
+    if (password !== rePassword) {
+      alert("Passwords do not match.");
       return;
     }
 
     try {
-      await signInWithEmailAndPassword(auth, email, password);
+      await createUserWithEmailAndPassword(auth, email, password, rePassword);
       navigate("/");
     } catch (err) {
       console.error(err);
     }
   };
-
   return (
     <div className="login-container">
-      <h2>Sign in to your account</h2>
+      <h2>Register</h2>
       <div className="inputs">
         <input
           type="email"
@@ -41,13 +44,20 @@ export default function Login() {
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
-      <button className="login-btn" onClick={handleLogin}>
-        Login
+      <div className="inputs">
+        <input
+          type="Password"
+          placeholder="Repassword.."
+          onChange={(e) => setRePassword(e.target.value)}
+        />
+      </div>
+      <button className="login-btn" onClick={handleRegister}>
+        Register
       </button>
       <p className="case">
-        Don't have a profile?
-        <Link className="register-link" to="/register">
-          Register
+        Already have a profile?
+        <Link className="register-link" to="/login">
+          Log in
         </Link>
       </p>
     </div>

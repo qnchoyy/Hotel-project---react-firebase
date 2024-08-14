52c1eb5e51e6e4b07ee5dc2de9c035bbe3541ee0
qnchoyy <qni191488@abv.bg>
2024-08-15 02:33:10 +0300

    add logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
+import hotelLogo from "../../images/hotelLogo.png";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <Link to="/">My Hotel</Link>
+        <Link to="/">
+          <img className={styles.logo} src={hotelLogo} alt="hotelLogo" />
+        </Link>
       </div>
       <nav className={styles.nav}>
         <ul>
